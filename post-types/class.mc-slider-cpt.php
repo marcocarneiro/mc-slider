13a039ce1e5b7edd6af67bdc32768d0876eb83f2
marcocarneiro <marco.carneiro7@gmail.com>
2023-03-21 21:44:38 -0300

    Implementação de metaboxes para o Custom Post Type

--- post-types/class.mc-slider-cpt.php
+++ post-types/class.mc-slider-cpt.php
@@ -4,6 +4,8 @@ if( ! class_exists( 'MC_Slider_Post_Type')){
     class MC_Slider_Post_Type{
         function __construct(){
             add_action( 'init', array( $this, 'create_post_type' ) );
+            add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes') );
+            add_action( 'save_post', array( $this, 'save_post' ), 10, 2 );
         }
 
         public function create_post_type(){
@@ -11,7 +13,7 @@ if( ! class_exists( 'MC_Slider_Post_Type')){
                 'mc-slider',
                 array(
                     'label' => 'Slider',
-                    'description' => 'Descrição Slideers',
+                    'description' => 'Descrição Sliders',
                     'labels' => array(
                         'name' => 'Sliders',
                         'singular_name' => 'Slider'
@@ -32,8 +34,35 @@ if( ! class_exists( 'MC_Slider_Post_Type')){
                     'show_in_rest' => true,
                     'menu_icon' => 'dashicons-images-alt2'
                 )
+            );            
+        }
+
+        public function add_meta_boxes(){
+            add_meta_box( 
+                'mc_slider_meta_box',
+                'Link Options',
+                array( $this, 'add_inner_meta_boxes'),
+                'mc-slider',
+                'normal',
+                'high',
             );
-            
+        }
+
+        public function add_inner_meta_boxes( $post ){
+            //formulário HTML
+            require_once( MC_SLIDER_PATH . 'views/mc-slider_metabox.php' );
+        }
+
+        public function save_post( $post_id ){
+            if( isset( $_POST['action']) && $_POST['action'] == 'editpost'){
+                $old_link_text = get_post_meta( $post_id, 'mc_slider_link_text', true );
+                $new_link_text = $_POST['mc_slider_link_text'];
+                $old_link_url  = get_post_meta( $post_id, 'mc_slider_link_url', true );
+                $new_link_url = $_POST['mc_slider_link_url'];
+
+                update_post_meta( $post_id, 'mc_slider_link_text', $new_link_text, $old_link_text);
+                update_post_meta( $post_id, 'mc_slider_link_url', $new_link_url, $old_link_url);
+            }
         }
 
     }
