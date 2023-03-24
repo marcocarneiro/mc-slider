e6f40cb256dd302cb63b28de7203801d022d1f0c
marcocarneiro <marco.carneiro7@gmail.com>
2023-03-24 17:11:20 -0300

    Implementação de colunas no admin

--- post-types/class.mc-slider-cpt.php
+++ post-types/class.mc-slider-cpt.php
@@ -6,6 +6,15 @@ if( ! class_exists( 'MC_Slider_Post_Type')){
             add_action( 'init', array( $this, 'create_post_type' ) );
             add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes') );
             add_action( 'save_post', array( $this, 'save_post' ), 10, 2 );
+
+            //Show other columns on admin panel. Filter manage_{cpt-name}_posts_columns
+            add_filter('manage_mc-slider_posts_columns', array( $this, 'mc_slider_cpt_columns'));
+            add_action( 'manage_mc-slider_posts_custom_column', array( $this, 'mc_slider_custom_columns'), 10, 2);
+            //enable sortable CPT columns
+            add_filter( 'manage_edit-mc-slider_sortable_columns', array( $this, 'mc_sortable_columns') );
+            //image thumbnail column
+            add_filter('manage_posts_columns', array( $this, 'posts_columns' ), 5);
+            add_action('manage_posts_custom_column', array( $this, 'posts_custom_columns' ), 5, 2);          
         }
 
         public function create_post_type(){
@@ -37,6 +46,43 @@ if( ! class_exists( 'MC_Slider_Post_Type')){
             );            
         }
 
+        public function mc_slider_cpt_columns( $columns ){
+            $columns['mc_slider_link_text'] = esc_html__('Link Text', 'mc-slider');
+            $columns['mc_slider_link_url'] = esc_html__('Link URL', 'mc-slider');
+            return $columns;
+        }
+
+        public function mc_slider_custom_columns( $column, $post_id){
+            switch( $column ){
+                case 'mc_slider_link_text':
+                    echo esc_html( get_post_meta( $post_id, 'mc_slider_link_text', true ));
+                break;
+                case 'mc_slider_link_url':
+                    echo esc_url( get_post_meta( $post_id, 'mc_slider_link_url', true ));
+                break;
+            }
+        }
+
+        public function mc_sortable_columns( $columns ){
+            $columns['mc_slider_link_text'] = 'mc_slider_link_text';
+            return $columns;
+        }
+
+        //show column image thumbnails methods
+        public function posts_columns($defaults){            
+            $defaults['my_post_thumbs'] = __('Imagem');
+            return $defaults;
+        }
+        public function posts_custom_columns($column_name, $id){
+            add_image_size( 'admin-thumb', 100, 999999 );
+            if($column_name === 'my_post_thumbs'){
+                echo the_post_thumbnail( 'admin-thumb' );
+            }
+        }
+
+
+
+
         public function add_meta_boxes(){
             add_meta_box( 
                 'mc_slider_meta_box',
@@ -46,7 +92,7 @@ if( ! class_exists( 'MC_Slider_Post_Type')){
                 'normal',
                 'high',
             );
-        }
+        }        
 
         public function add_inner_meta_boxes( $post ){
             //formulário HTML
